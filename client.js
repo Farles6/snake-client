b6f25f59d434c3482bda381b12dd41ada598660f
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-03-16 13:48:24 -0400

    tested out moving

--- client.js
+++ client.js
@@ -1,18 +1,28 @@
 const net = require('net');
+
 const connect = () => {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
   });
+
   conn.setEncoding('utf8');
   conn.on('data', (data) => {
     console.log(data)
   })
   conn.on('connect', (connect) => {
-console.log('Finally connected');
+    let timer = 0;
+    console.log('Finally connected');
+    conn.write('Name: FAR');
+   
   });
-  conn.write('Name: FAR')
+
+
   return conn;
 };
 
 module.exports = connect;
+
+// setInterval(() => {
+//   conn.write('Move: up');
+// }, 50)
